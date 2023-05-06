9d965efb068c01a5f4c3e020b650496cfdbe2e13
windsing <117965575+wochenlong@users.noreply.github.com>
2023-05-06 13:10:30 +0800

    Update index.ts
    最新的简约版

--- sd-list/src/index.ts
+++ sd-list/src/index.ts
@@ -2,7 +2,6 @@ import { Context, Logger, Schema, trimSlash } from "koishi";
 
 export const name = "sd-list";
 
-export const usage = "查看模型、pt、lora列表";
 export interface Config {
   endpoint?: string;
 }
@@ -20,52 +19,33 @@ const PT_ENDPOINT = "/sdapi/v1/embeddings";
 
 export function apply(ctx: Context, config: Config) {
   const cmd1 = ctx
-    .command("查看模型", "ai绘画的模型列表")
-    .alias("获取模型列表")
+    .command("查看model", "ai绘画的model列表")
     .action(async ({ session }) => {
-      const models = [];
-      let vae = "";
-      let model = "";
+      let modelTitles = [];
 
       async function getModelList() {
         try {
-          const res = await ctx.http.axios(
+          const res = await ctx.http.get(
             trimSlash(config.endpoint) + MODELS_ENDPOINT
           );
-          res.data.forEach((item) => models.push(item.title));
+          modelTitles = res.map((model) => model.title);
         } catch (err) {
-          session.send(session.text("获取模型列表失败"));
+          session.send(session.text("获取model列表失败"));
           throw err;
         }
       }
 
-      // 定义函数 sendModels，该函数向用户发送可用的模型列表
       async function sendModels() {
-        // 向 API 发送请求获取模型列表
         await getModelList();
-        // 向 API 发送请求获取当前使用的模型
-        await getInfo();
         session.send(
-          "当前可用模型有：\n" +
-            models.map((model, i) => `${i + 1}.${model}\n`).join("")
+          `当前可用model有：\n${modelTitles
+            .map((title, i) => `${i + 1}.${title}`)
+            .join("\n")}`
         );
       }
-      async function getInfo() {
-        session.send(session.text(".inQuery"));
-        try {
-          const res = await ctx.http.axios(
-            trimSlash(config.endpoint) + MODELS_ENDPOINT
-          );
-          vae = res.data.sd_vae;
-          model = res.data.sd_model_checkpoint;
-        } catch (err) {
-          session.send(session.text(".queryErr"));
-          throw err;
-        }
-      }
+
       await sendModels();
     });
-
 
   const cmd2 = ctx
     .command("查看pt", "ai绘画的pt列表")
