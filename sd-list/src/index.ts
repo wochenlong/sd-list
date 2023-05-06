331a79e1431d994202da526eb81887e380d2e3ce
windsing <117965575+wochenlong@users.noreply.github.com>
2023-05-06 12:43:26 +0800

    Update index.ts
    现在可以请求到pt 列表

--- sd-list/src/index.ts
+++ sd-list/src/index.ts
@@ -1,6 +1,6 @@
 import { Context, Logger, Schema, trimSlash } from "koishi";
 
-export const name = "sd-switch";
+export const name = "sd-list";
 
 export const usage = "查看模型、pt、lora列表";
 export interface Config {
@@ -20,13 +20,12 @@ const PT_ENDPOINT = "/sdapi/v1/embeddings";
 
 export function apply(ctx: Context, config: Config) {
   const cmd1 = ctx
-    .command("查看绘画模型", "ai绘画的模型列表")
+    .command("查看模型", "ai绘画的模型列表")
     .alias("获取模型列表")
     .action(async ({ session }) => {
       const models = [];
       let vae = "";
       let model = "";
-      const pt = [];
 
       async function getModelList() {
         try {
@@ -40,48 +39,57 @@ export function apply(ctx: Context, config: Config) {
         }
       }
 
-      async function getPtList() {
+      // 定义函数 sendModels，该函数向用户发送可用的模型列表
+      async function sendModels() {
+        // 向 API 发送请求获取模型列表
+        await getModelList();
+        // 向 API 发送请求获取当前使用的模型
+        await getInfo();
+        session.send(
+          "当前可用模型有：\n" +
+            models.map((model, i) => `${i + 1}.${model}\n`).join("")
+        );
+      }
+      async function getInfo() {
+        session.send(session.text(".inQuery"));
         try {
           const res = await ctx.http.axios(
+            trimSlash(config.endpoint) + MODELS_ENDPOINT
+          );
+          vae = res.data.sd_vae;
+          model = res.data.sd_model_checkpoint;
+        } catch (err) {
+          session.send(session.text(".queryErr"));
+          throw err;
+        }
+      }
+      await sendModels();
+    });
+
+
+  const cmd2 = ctx
+    .command("查看pt", "ai绘画的pt列表")
+    .action(async ({ session }) => {
+      let pt = [];
+
+      async function getPtList() {
+        try {
+          const res = await ctx.http.get(
             trimSlash(config.endpoint) + PT_ENDPOINT
           );
-          res.data.forEach((item) => pt.push(item.title));
+          pt = Object.keys(res.loaded);
         } catch (err) {
           session.send(session.text("获取pt列表失败"));
           throw err;
         }
       }
-
-      async function sendModels() {
-        await getModelList();
-        session.send(`
-          当前可用模型有：
-          ${models.map((model, i) => `${i + 1}.${model}`).join("\n")}
-        `);
-      }
-
       async function sendPt() {
         await getPtList();
-        session.send(`
-          当前可用pt有：
-          ${pt.map((pt, i) => `${i + 1}.${pt}`).join("\n")}
-        `);
+        session.send(
+          `当前可用pt有：\n${pt.map((pt, i) => `${i + 1}.${pt}`).join("\n")}`
+        );
       }
 
-      const cmd2 = ctx.command("查看pt", "ai绘画的pt列表").action(async () => {
-        await sendPt();
-      });
-
-      await sendModels();
+      await sendPt();
     });
-
-
-
-
-
-
-
-
-
-    
 }
