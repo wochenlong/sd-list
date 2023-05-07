80c98d461da336309a3f577bfa3fa1e8341a2b50
windsing <117965575+wochenlong@users.noreply.github.com>
2023-05-07 18:04:31 +0800

    Update index.ts
    lora列表现在可以以图片的形式发送（依赖pptr服务）

--- sd-list/src/index.ts
+++ sd-list/src/index.ts
@@ -1,28 +1,20 @@
-import { Context, Logger, Schema, trimSlash } from "koishi";
+import { Context, Schema, Logger } from "koishi";
 import * as fs from "fs";
 import * as path from "path";
 
-export const name = "sd-list";
+export const name = "lora";
 
-export const usage = `
-
-查看模型、pt、lora列表
-
-[意见反馈](https://forum.koishi.xyz/t/topic/2018)
-
-`;
 export interface Config {
-  endpoint?: string;
   folderPath?: string;
   loraweights?: number;
   maxCount?: number;
+  sendAsImage?: boolean;
 }
 
 export const Config: Schema<Config> = Schema.object({
-  endpoint: Schema.string()
-    .description("SD-WebUI 服务器地址。")
-    .default("http://127.0.0.1:7860"),
-  folderPath: Schema.string().description("本地的lora文件夹路径。").default(""),
+  folderPath: Schema.string()
+    .description("lora文件夹路径。")
+    .default("D:/a压缩包/lora"),
   loraweights: Schema.number()
     .description("输出lora的默认权重")
     .min(0)
@@ -30,74 +22,18 @@ export const Config: Schema<Config> = Schema.object({
     .step(0.1)
     .default(0.6),
   maxCount: Schema.number()
-    .description("每段最多包含的lora文件数量")
+    .description("每段最多包含的文件数量")
     .min(1)
     .max(50)
     .step(1)
     .default(20),
+  sendAsImage: Schema.boolean()
+    .description("是否以图片形式发送")
+    .default(false),
 });
 
 const logger = new Logger(name);
-
-const MODELS_ENDPOINT = "/sdapi/v1/sd-models";
-const PT_ENDPOINT = "/sdapi/v1/embeddings";
-
 export function apply(ctx: Context, config: Config) {
-  const cmd1 = ctx
-    .command("查看模型", "ai绘画的model列表")
-    .action(async ({ session }) => {
-      let modelTitles = [];
-
-      async function getModelList() {
-        try {
-          const res = await ctx.http.get(
-            trimSlash(config.endpoint) + MODELS_ENDPOINT
-          );
-          modelTitles = res.map((model) => model.title);
-        } catch (err) {
-          session.send(session.text("获取model列表失败"));
-          throw err;
-        }
-      }
-
-      async function sendModels() {
-        await getModelList();
-        session.send(
-          `当前可用model有：\n${modelTitles
-            .map((title, i) => `${i + 1}.${title}`)
-            .join("\n")}`
-        );
-      }
-
-      await sendModels();
-    });
-
-  const cmd2 = ctx
-    .command("查看pt", "ai绘画的pt列表")
-    .action(async ({ session }) => {
-      let pt = [];
-
-      async function getPtList() {
-        try {
-          const res = await ctx.http.get(
-            trimSlash(config.endpoint) + PT_ENDPOINT
-          );
-          pt = Object.keys(res.loaded);
-        } catch (err) {
-          session.send(session.text("获取pt列表失败"));
-          throw err;
-        }
-      }
-      async function sendPt() {
-        await getPtList();
-        session.send(
-          `当前可用pt有：\n${pt.map((pt, i) => `${i + 1}.${pt}`).join("\n")}`
-        );
-      }
-
-      await sendPt();
-    });
-
   const cmd3 = ctx
     .command("查看lora", "输出lora文件夹下的模型名称")
     .action(async ({ session }) => {
@@ -118,19 +54,41 @@ export function apply(ctx: Context, config: Config) {
           );
         if (fileList.length === 0) {
           session.send(`没有找到符合条件的文件`);
-        } else {
-          // 将文件名列表分段发送到会话中
-          const maxCount = config.maxCount; // 每段最多包含的文件数量
-          const sectionCount = Math.ceil(fileList.length / maxCount); // 计算需要分成几段发送
-          for (let i = 0; i < sectionCount; i++) {
-            const sectionList = fileList.slice(
-              i * maxCount,
-              (i + 1) * maxCount
-            ); // 获取当前段需要发送的文件列表
-            session.send(
-              `lora列表（第 ${i + 1} 段）：\n${sectionList.join("\u200B\n")}`
-            );
-          }
+          return;
+        }
+
+        // 将文件名列表分段发送到会话中
+        const maxCount = config.maxCount; // 每段最多包含的文件数量
+        const sectionCount = Math.ceil(fileList.length / maxCount); // 计算需要分成几段发送
+        for (let i = 0; i < sectionCount; i++) {
+          const sectionList = fileList.slice(i * maxCount, (i + 1) * maxCount); // 获取当前段需要发送的文件列表
+          const list = `\n${sectionList.join("\u200B\n")}`;
+          const message = config.sendAsImage
+            ? `
+          <html>
+            <style>
+              .container {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+              }
+              .title {
+                font-size: 20px;
+                font-weight: bold;
+                margin-top: 10px;
+              }
+              .list {
+                white-space: pre-wrap;
+              }
+            </style>
+            <div class="container">
+              <div class="title">我的lora</div>
+              <div class="list">${list}</div>
+            </div>
+          </html>
+        `
+            : `lora列表（第 ${i + 1} 段）：\n${sectionList.join("\u200B\n")}`;
+          session.send(message);
         }
       } catch (err) {
         // 如果读取文件列表失败，则发送错误消息
