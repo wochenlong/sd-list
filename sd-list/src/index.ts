32c250b7e6279930e4eff53bee9e5c4fe0f2f6b1
windsing <117965575+wochenlong@users.noreply.github.com>
2023-05-07 16:21:02 +0800

    Update index.ts
    增加了对文件后缀的筛选，以及可以分段发送

--- sd-list/src/index.ts
+++ sd-list/src/index.ts
@@ -15,6 +15,7 @@ export interface Config {
   endpoint?: string;
   folderPath?: string;
   loraweights?: number;
+  maxCount?: number;
 }
 
 export const Config: Schema<Config> = Schema.object({
@@ -28,6 +29,12 @@ export const Config: Schema<Config> = Schema.object({
     .max(2)
     .step(0.1)
     .default(0.6),
+  maxCount: Schema.number()
+    .description("每段最多包含的lora文件数量")
+    .min(1)
+    .max(50)
+    .step(1)
+    .default(20),
 });
 
 const logger = new Logger(name);
@@ -101,12 +108,30 @@ export function apply(ctx: Context, config: Config) {
         // 读取文件夹中所有文件的文件名
         const files = await fs.promises.readdir(folderPath);
         // 对每个文件名进行处理，添加序号和<lora:>标记
-        const fileList = files.map(
-          (file, i) =>
-            `${i + 1}. <lora:${path.parse(file).name}:${loraweights}>`
-        );
-        // 将文件名列表发送到会话中
-        session.send(`lora列表：\n${fileList.join("\u200B\n")}`);
+        const fileList = files
+          .filter((file) =>
+            [".safetensors", ".pt"].includes(path.parse(file).ext)
+          )
+          .map(
+            (file, i) =>
+              `${i + 1}. <lora:${path.parse(file).name}:${loraweights}>`
+          );
+        if (fileList.length === 0) {
+          session.send(`没有找到符合条件的文件`);
+        } else {
+          // 将文件名列表分段发送到会话中
+          const maxCount = config.maxCount; // 每段最多包含的文件数量
+          const sectionCount = Math.ceil(fileList.length / maxCount); // 计算需要分成几段发送
+          for (let i = 0; i < sectionCount; i++) {
+            const sectionList = fileList.slice(
+              i * maxCount,
+              (i + 1) * maxCount
+            ); // 获取当前段需要发送的文件列表
+            session.send(
+              `lora列表（第 ${i + 1} 段）：\n${sectionList.join("\u200B\n")}`
+            );
+          }
+        }
       } catch (err) {
         // 如果读取文件列表失败，则发送错误消息
         session.send(`读取文件夹 ${folderPath} 失败`);
